077a20002d417d39bc649ce6feaf900515d8e5f9
niart120 <38847256+niart120@users.noreply.github.com>
2023-07-15 01:45:02 +0900

    Update BlinkCapturerer.cs

--- BlinkReader/BlinkCapturerer.cs
+++ BlinkReader/BlinkCapturerer.cs
@@ -14,7 +14,8 @@ namespace BlinkReader
 
             while (!token.IsCancellationRequested)
             {
-                await Task.Delay((int)(1000/fps));
+                //最低1msはビジーウェイトさせるようにする
+                await Task.Delay((int)(1000/fps) - 1);
                 var currentTick = DateTime.Now.Ticks;
                 while (currentTick < nextFrame) currentTick = DateTime.Now.Ticks;
                 nextFrame += TPF;
@@ -26,6 +27,25 @@ namespace BlinkReader
                     yield return currentTick;
                 }
                 isBlinked = isBlinking;
+                bmp.Dispose();
+            }
+        }
+
+        public static async IAsyncEnumerable<bool> CaptureBlinkTestAsync([EnumeratorCancellation] CancellationToken token, IDetector detector, CaptureWindowForm windowForm, double fps = 60.0)
+        {
+            NativeMethods.Winmm.timeBeginPeriod(1);
+            long TPF = (long)(10_000_000 / fps);//Ticks Per Frame
+            var nextFrame = DateTime.Now.Ticks;
+
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay((int)(1000 / fps));
+                var currentTick = DateTime.Now.Ticks;
+                while (currentTick < nextFrame) currentTick = DateTime.Now.Ticks;
+                nextFrame += TPF;
+
+                var bmp = windowForm.CaptureScreen();
+                yield return detector.Detect(bmp);
                 bmp.Dispose();
             }
         }
